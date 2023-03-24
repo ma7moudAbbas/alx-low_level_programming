311caecd75f89f397cb8e622790f66d9e9664c2c
mahmoud abbas <mahmoudabbasmohamed999@gmail.com>
2023-03-24 14:15:35 +0200

    create is upper function

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,4 @@
-#include <studio.h>
-
+#include <stdio.h>
 int _isupper(int c)
 {
 	if(c >= 'A' && c <= 'B')
@@ -10,6 +9,11 @@ int _isupper(int c)
 
 int main()
 {
-
+	char c;
+    	c = 'A';
+    	printf("%c: %d\n", c, _isupper(c));
+    	c = 'a';
+    	printf("%c: %d\n", c, _isupper(c));
+    	return (0);
 }
 
